d325f64bd5c95876bb52d0070e665e3f3aa26881
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-04 14:35:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Crea un nuovo array con la lista dei mammiferi.
+*/
+
+// CREP ARRAY 
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi'},
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi'},
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli'},
+    { nome: 'gatto', famiglia: 'felidi', classe: 'mammiferi'},
+    { nome: 'cavallo', famiglia: 'equidi', classe: 'mammiferi'},
+]
+
